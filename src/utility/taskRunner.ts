f6d489386b9b1261f2597665a84af31f76787aa0
James Hill <jahill@groupon.com>
2020-04-01 16:34:30 +0100

    feat: [VC-1631] - Refactored task runner to support watching

--- src/utility/taskRunner.ts
+++ src/utility/taskRunner.ts
@@ -1,4 +1,5 @@
-import { tasks, Method } from '@/tasks';
+import chokidar from 'chokidar';
+import { tasks, Method, Task } from '@/tasks';
 import { IOptions } from '@/options';
 import * as log from '@/utility/logOutput';
 import { readFile } from '@/utility/readFile';
@@ -29,7 +30,7 @@ async function taskRunner(
 ) {
    const startTime = new Date().getTime();
    const paths = await readGlobFiles(sourcePath);
-   const method: Method = tasks[methodKey];
+   const taskMethod = () => runTask(tasks[methodKey], paths, options);
 
    if (!paths.length) {
       log.error('No matching files for', sourcePath);
@@ -37,15 +38,41 @@ async function taskRunner(
       return;
    }
 
-   const files = paths.map((item) => readFile(item));
-   const build = await method(options);
-
    log.info('Running', methodKey, 'task...');
+
+   await taskMethod();
+
+   const endTime = new Date().getTime();
+
+   log.info('Finished', methodKey, `after ${endTime - startTime} ms`);
+
+   if (options.watch) {
+      chokidar
+         .watch(sourcePath)
+         .on('change', async () => watchTask(taskMethod));
+   }
+}
+
+/* -----------------------------------
+ *
+ * Task
+ *
+ * -------------------------------- */
+
+async function runTask(
+   method: Method,
+   paths: string[],
+   options: IOptions
+) {
+   const files = paths.map((item) => readFile(item));
+   const task = await method(options);
+
+   let result = [];
 
    try {
       const streams = await Promise.all(
          files.map((stream, index) =>
-            build({
+            task({
                stream,
                path: paths[index],
                name: getFileName(stream),
@@ -53,22 +80,26 @@ async function taskRunner(
          )
       );
 
-      const result = await writeResultStreams(streams, options.output);
-
-      if (options.verbose) {
-         result.forEach(log.result);
-      }
+      result = await writeResultStreams(streams, options.output);
    } catch ({ message, file, line }) {
       log.error(message, file, line);
 
       process.exit(1);
-
-      return;
    }
 
-   const endTime = new Date().getTime();
+   if (options.verbose) {
+      result.forEach(log.result);
+   }
+}
 
-   log.info('Finished', methodKey, `after ${endTime - startTime} ms`);
+/* -----------------------------------
+ *
+ * Watch
+ *
+ * -------------------------------- */
+
+async function watchTask(task: () => Promise<void>) {
+   //
 }
 
 /* -----------------------------------
