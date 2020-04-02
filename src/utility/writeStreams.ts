a77b32bf5a85ddcdde275bde63a968a49ac5cf73
James Hill <me@jhdev.uk>
2020-04-02 20:00:14 +0100

    feat: [VC-1631] - Added mkdir to write streams function

--- src/utility/writeStreams.ts
+++ src/utility/writeStreams.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import mkdir from 'mkdirp';
 import { IResult } from '@/tasks';
 import { writeFile } from '@/utility/writeFile';
 
@@ -12,6 +13,8 @@ async function writeStreams(
    streams: IResult[],
    output: string
 ): Promise<IResult[]> {
+   await mkdir(output);
+
    await Promise.all(
       streams.map(({ name, stream }) =>
          writeFile(path.join(output, name), stream)
