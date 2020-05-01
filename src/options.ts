bd1e498d06cf0b6ca48a2f5578b9bc1990954931
James Hill <me@jhdev.uk>
2020-05-01 18:32:10 +0100

    Updated default webpack config

--- src/options.ts
+++ src/options.ts
@@ -12,11 +12,11 @@ interface IOptions {
    sourceMap: boolean;
    cssModules: boolean;
    watch: boolean;
-   watchPath: string;
+   watchPath?: string;
    verbose: boolean;
    pathAlias: string;
-   filePrefix: string;
-   includePath: string;
+   filePrefix?: string;
+   includePath?: string;
    localWebpackConfig?: (options: IOptions) => Configuration;
 }
 
--- src/tasks/webpack/tools/webpack.default.ts
+++ src/tasks/webpack/tools/webpack.default.ts
@@ -8,78 +8,76 @@ import { IOptions } from '@/options';
  *
  * -------------------------------- */
 
-const defaultWebpackConfig = ({
+function defaultWebpackConfig({
    release,
    pathAlias,
    includePath,
-}: IOptions): Configuration => ({
-   target: 'web',
-   mode: release ? 'production' : 'development',
-   // cache: !release,
-   output: {
-      libraryTarget: 'commonjs',
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-      jsonpFunction: '__IDL__',
-   },
-   resolve: {
-      modules: [
-         'node_modules',
-         ...(includePath && [path.resolve(includePath)]),
-      ],
-      extensions: ['.ts', '.tsx', '.js', '.json'],
-      alias: {
-         ...(pathAlias && {
+}: IOptions): Configuration {
+   return {
+      target: 'web',
+      mode: release ? 'production' : 'development',
+      // cache: !release,
+      output: {
+         libraryTarget: 'commonjs',
+         filename: '[name].js',
+         chunkFilename: '[name].js',
+         jsonpFunction: '__IDL__',
+      },
+      resolve: {
+         modules: [
+            'node_modules',
+            ...(includePath ? [path.resolve(includePath)] : []),
+         ],
+         extensions: ['.ts', '.tsx', '.js', '.json'],
+         alias: {
             '@': path.resolve(pathAlias),
-         }),
-      },
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            enforce: 'pre',
-            loader: 'tslint-loader',
-            options: {
-               fix: true,
-            },
          },
-         {
-            test: /\.tsx?$/,
-            ...(includePath && {
-               include: path.resolve(includePath),
-            }),
-            use: [
-               {
-                  loader: 'ts-loader',
+      },
+      module: {
+         rules: [
+            {
+               test: /\.tsx?$/,
+               enforce: 'pre',
+               loader: 'tslint-loader',
+               options: {
+                  fix: true,
                },
-            ],
-         },
-      ],
-   },
-   optimization: {
-      mergeDuplicateChunks: true,
-      runtimeChunk: false,
-      splitChunks: {
-         name: true,
-         chunks: 'async',
-         cacheGroups: {
-            default: false,
-            commons: {
-               name: 'shared',
-               minChunks: 2,
-               maxInitialRequests: 5,
             },
-            vendor: {
-               test: /[\\/]node_modules[\\/]/,
-               name: 'vendor',
-               enforce: true,
-               chunks: 'all',
+            {
+               test: /\.tsx?$/,
+               include: includePath ? path.resolve(includePath) : void 0,
+               use: [
+                  {
+                     loader: 'ts-loader',
+                  },
+               ],
+            },
+         ],
+      },
+      optimization: {
+         mergeDuplicateChunks: true,
+         runtimeChunk: false,
+         splitChunks: {
+            name: true,
+            chunks: 'async',
+            cacheGroups: {
+               default: false,
+               commons: {
+                  name: 'shared',
+                  minChunks: 2,
+                  maxInitialRequests: 5,
+               },
+               vendor: {
+                  test: /[\\/]node_modules[\\/]/,
+                  name: 'vendor',
+                  enforce: true,
+                  chunks: 'all',
+               },
             },
          },
       },
-   },
-});
+   };
+}
 
 /* -----------------------------------
  *
