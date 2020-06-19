9fc6435fba1f78ff3a8dc812bf32a1f052ce20fc
James Hill <me@jhdev.uk>
2020-06-19 14:17:48 +0100

    Finished CSProj task

--- src/tasks/csproj/tools/buildResult.ts
+++ src/tasks/csproj/tools/buildResult.ts
@@ -1,4 +1,5 @@
 import { ReadStream } from 'fs';
+import path from 'path';
 import libxml from 'libxmljs';
 import { stringToStream } from '@/utility/streamHelpers';
 import { parseXMLDoc } from './parseXMLDoc';
@@ -10,16 +11,20 @@ import { config } from './config';
  *
  * -------------------------------- */
 
-async function buildResult(manifest: object, data: ReadStream) {
+async function buildResult(
+   manifestPath: string,
+   manifest: object,
+   data: ReadStream
+) {
    const xmlDoc = await parseXMLDoc(data);
    const fileNames = Object.keys(manifest);
 
    fileNames.forEach((file) => {
       const element: any = new libxml.Element(xmlDoc, config.element);
 
-      element.attr(config.attribute, file);
+      element.attr(config.attribute, path.join(manifestPath, file));
 
-      xmlDoc.get(config.xpath, { xmlns: config.xmlns });
+      xmlDoc.get(config.xpath, { xmlns: config.xmlns }).addChild(element);
    });
 
    return stringToStream(xmlDoc.toString());
